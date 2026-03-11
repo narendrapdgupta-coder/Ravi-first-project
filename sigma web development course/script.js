e1467ba1e01cd66a605efbc0e0f568a4a19b090e
narendrapdgupta-coder <narendrapdgupta@gmail.com>
2026-03-11 18:59:21 +0530

    index.html style.css script.js images folder
    index.html
    style.css
    script.js
    images folder

---
+++ sigma web development course/script.js
@@ -0,0 +1,13 @@
+function consult(){
+
+alert("Thank you for contacting LIC Advisor Narendra Prasad Gupta!");
+
+}
+
+function sendForm(event){
+
+event.preventDefault();
+
+alert("Your enquiry has been submitted successfully!");
+
+}
